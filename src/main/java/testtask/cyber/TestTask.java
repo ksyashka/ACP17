b811c5100def8b842d9e15d27bfc7d6844f252e8
ksyashka <ksyashka19@gmail.com>
2017-10-05 17:19:58 +0300

    last commit 0510

--- src/main/java/testtask/cyber/TestTask.java
+++ src/main/java/testtask/cyber/TestTask.java
@@ -3,45 +3,44 @@ package testtask.cyber;
 import java.util.Arrays;
 
 public class TestTask {
-    static String sumString(String a, String b){
+    static String sumString(String a, String b) {
         int valueA = Integer.valueOf(a);//Integer.parseInt(a);
         int valueB = Integer.valueOf(b);
-        int result = valueA+valueB;
+        int result = valueA + valueB;
         return String.valueOf(result);//Integer.toString(result);
     }
 
-    static int maxCountSequence(int ... numbers){
-        int count=1, countMax=1;
-        for (int i=1; i<numbers.length;i++){
-            if (numbers[i]>numbers[i-1]) {
+    static int maxCountSequence(int... numbers) {
+        int count = 1, countMax = 1;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[i - 1]) {
                 count++;
                 if (count > countMax) countMax = count;
-            }
-            else count=1;
+            } else count = 1;
         }
         return countMax;
     }
 
-    static void showRandom(int number){
+    static void showRandom(int number) {
         int[] numbers = new int[number];
-        for (int i=0;i<number;i++)
-            numbers[i]=i+1;
+        for (int i = 0; i < number; i++)
+            numbers[i] = i + 1;
 
-        for (int i=0;i<number;i++)
-            for(int j=1;j<number;j++) {
+        for (int i = 0; i < number; i++)
+            for (int j = 1; j < number; j++) {
                 System.out.println(Arrays.toString(numbers));
-                int temp=numbers[i];
-                numbers[i]=numbers[j];
-                numbers[j]=temp;
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
             }
 
     }
 
-    static void order(int ... numbers){
-       // Arrays.sort(numbers);
-        for (int i=0; i<numbers.length;i++)
-            for (int j=0; j<numbers.length;j++)
-                if(numbers[j]>numbers[i]) {
+    static void order(int... numbers) {
+        // Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length; i++)
+            for (int j = 0; j < numbers.length; j++)
+                if (numbers[j] > numbers[i]) {
                     int temp = numbers[i];
                     numbers[i] = numbers[j];
                     numbers[j] = temp;
@@ -49,30 +48,29 @@ public class TestTask {
         System.out.println(Arrays.toString(numbers));
     }
 
-    static int min(int[] a, int k){
-        return 0;
+    static int fibonacci(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        return fibonacci(n-2)+fibonacci(n-1);
     }
 
-    static boolean contains(int[] a, int value){
-        return false;
-    }
-
-    static void printSum(int[] a, int sum){
+    static void printSum(int[] a, int sum) {
 
     }
 
-    static void printMatrix(int n){
-        if (n == 0) System.exit(0);
-        for (int i=0;i<n;i++){
-            printStars(n-i);
-            System.out.print(i+1);
-            printStars(n-i);
+    static void printMatrix(int n) {
+        int print = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 2 - 1; j++) {
+
+                System.out.print(print);
+                if (j < n - 1 || print == 0) print++;
+                if (j >= n - 1 && print > 1) print--;
+            }
             System.out.println();
+        }
+
     }
 }
 
-    private static void printStars(int n) {
-        for (int i=0;i<n-1;i++)
-        System.out.print("*");
-    }
-    }
